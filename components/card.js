8205381f5f40900cab30ccf47736c2ce6966f47a
CingSharped <chrismsharpe123@gmail.com>
2022-08-14 21:48:10 +0100

    Removal off RTC stuff

--- components/card.js
+++
@@ -1,9 +0,0 @@
-export default function Card({currentCard}) {
-    return (
-        <div className="cardStyling">
-            <h1>Yu Gi Oh!</h1>
-            <h1>{currentCard ? currentCard.title : "No Title"}</h1>
-            <p>{currentCard ? currentCard.description : "No Description"}</p>
-        </div>
-    );
-}
